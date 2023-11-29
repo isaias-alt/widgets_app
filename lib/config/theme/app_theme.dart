0a508e983bd99a98e25dbc7f3f117b49447a8cae
Lucas Isaias Casco <cascolucasisaias@gmail.com>
2023-11-29 12:28:42 -0300

    add theme

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+const colorList = <Color>[
+  Colors.blue,
+  Colors.red,
+  Colors.orange,
+  Colors.purple,
+  Colors.green,
+  Colors.teal,
+  Colors.pink,
+  Colors.pinkAccent,
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({required this.selectedColor})
+      : assert(selectedColor >= 0 && selectedColor <= colorList.length - 1,
+            'Colors must be between 0 and ${colorList.length - 1}');
+
+  ThemeData getTheme() {
+    return ThemeData(
+      colorSchemeSeed: colorList[selectedColor],
+    );
+  }
+}
