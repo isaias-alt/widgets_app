c5a1453dc8cb084e0d3d97a4257a998dd0fd660a
isaias-alt <cascolucasisaias@gmail.com>
2023-12-19 17:41:40 -0300

    finish infiniteScroll and Pull to Refresh

--- lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
+++ lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -21,8 +21,9 @@ class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
   void initState() {
     super.initState();
     scrollController.addListener(() {
-      if ((scrollController.position.pixels + 500) >=
-          (scrollController.position.maxScrollExtent)) {
+      final double currentPosition = scrollController.position.pixels;
+      final double lastPosition = scrollController.position.maxScrollExtent;
+      if (currentPosition + 500 >= lastPosition) {
         loadNextPage();
       }
     });
@@ -39,18 +40,39 @@ class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
     if (isLoading) return;
     isLoading = true;
     setState(() {});
-
     await Future.delayed(const Duration(seconds: 1));
-
     addFiveImages();
     isLoading = false;
     if (!isMounted) return;
     setState(() {});
+    moveScrollToBottom();
   }
 
   void addFiveImages() {
     final lastId = imagesIds.last;
     imagesIds.addAll([1, 2, 3, 4, 5].map((e) => lastId + e));
+  }
+
+  Future<void> onRefresh() async {
+    isLoading = true;
+    setState(() {});
+    await Future.delayed(const Duration(seconds: 2));
+    if (!isMounted) return;
+    isLoading = false;
+    final lastId = imagesIds.last;
+    imagesIds.clear();
+    imagesIds.add(lastId + 1);
+    addFiveImages();
+    setState(() {});
+  }
+
+  void moveScrollToBottom() {
+    final double currentPosition = scrollController.position.pixels;
+    final double lastPosition = scrollController.position.maxScrollExtent;
+    if (currentPosition + 100 <= lastPosition) return;
+    scrollController.animateTo(currentPosition + 120,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.fastOutSlowIn);
   }
 
   @override
@@ -61,19 +83,12 @@ class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
         context: context,
         removeTop: true,
         removeBottom: true,
-        child: ListView.builder(
-          physics: const BouncingScrollPhysics(),
-          controller: scrollController,
-          itemCount: imagesIds.length,
-          itemBuilder: (context, index) {
-            return FadeInImage(
-                fit: BoxFit.cover,
-                width: double.infinity,
-                height: 300,
-                placeholder: const AssetImage('assets/images/jar-loading.gif'),
-                image: NetworkImage(
-                    'https://picsum.photos/id/${imagesIds[index]}/500/300'));
-          },
+        child: RefreshIndicator(
+          onRefresh: onRefresh,
+          edgeOffset: 10,
+          strokeWidth: 2,
+          child: _ListViewBuilder(
+              scrollController: scrollController, imagesIds: imagesIds),
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -83,6 +98,41 @@ class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
                 infinite: true, child: const Icon(Icons.refresh_rounded))
             : FadeIn(child: const Icon(Icons.arrow_back_ios_new_rounded)),
       ),
+    );
+  }
+}
+
+class _ListViewBuilder extends StatelessWidget {
+  const _ListViewBuilder({
+    required this.scrollController,
+    required this.imagesIds,
+  });
+
+  final ScrollController scrollController;
+  final List<int> imagesIds;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      physics: const BouncingScrollPhysics(),
+      controller: scrollController,
+      itemCount: imagesIds.length,
+      itemBuilder: (context, index) {
+        final networkImage = NetworkImage(
+            'https://picsum.photos/id/${imagesIds[index]}/500/300');
+        return FadeInImage(
+          fit: BoxFit.cover,
+          width: double.infinity,
+          height: 300,
+          placeholder: const AssetImage('assets/images/jar-loading.gif'),
+          image: networkImage,
+          imageErrorBuilder: (BuildContext context, error, stackTrace) =>
+              Image.asset(
+            'assets/images/image-not-aviable.png',
+            fit: BoxFit.cover,
+          ),
+        );
+      },
     );
   }
 }
