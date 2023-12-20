2355c8db97637834a205e0c334f81b57ad8f57a8
isaias-alt <cascolucasisaias@gmail.com>
2023-12-20 17:31:51 -0300

    add lateral menu

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
+import 'package:widgets_app/presentation/widgets/side_menu.dart';
 
 class HomeScreen extends StatelessWidget {
   static const String name = 'home_screen';
@@ -8,12 +9,14 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final scaffoldKey = GlobalKey<ScaffoldState>();
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Screen'),
-      ),
-      body: _HomeView(),
-    );
+        key: scaffoldKey,
+        appBar: AppBar(
+          title: const Text('Home Screen'),
+        ),
+        body: _HomeView(),
+        drawer: SideMenu(scaffoldKey: scaffoldKey));
   }
 }
 
